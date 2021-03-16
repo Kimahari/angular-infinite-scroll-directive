2dacfe3a84b648e75cdba130ab2ebdc1ba7a6271
Herman Esterhuizen <1herman.esterhuizen@gmail.com>
2021-03-16 20:58:18 +0200

    Changes

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,6 +8,6 @@ export class AppPage {
   getTitleText(): Promise<string> {
     return element(
       by.css('app-root .content span')
-    ).getText() as Promise<string>;
+    ).getText() as Promise<string>; 
   }
 }
