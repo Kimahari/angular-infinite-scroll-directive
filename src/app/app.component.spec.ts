da4a16e607506f0ad7976d58f8f61e2baa05a328
Herman Esterhuizen <1herman.esterhuizen@gmail.com>
2022-06-09 11:53:00 +0200

    Unit test cleanup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,20 @@
-import { AppComponent } from './app.component';
-import { Spectator, createComponentFactory } from '@ngneat/spectator';
-import { HttpClient } from '@angular/common/http';
+import { AppComponent } from "./app.component";
+import { Spectator, createComponentFactory } from "@ngneat/spectator";
+import { HttpClient } from "@angular/common/http";
+import { InfiniteScrollerDirective } from "./infinite-scroller.directive";
 
-describe('nameComponent', () => {
+describe("nameComponent", () => {
   let spectator: Spectator<AppComponent>;
 
   const createComponent = createComponentFactory({
     component: AppComponent,
     mocks: [HttpClient],
+    declarations: [InfiniteScrollerDirective],
   });
 
   beforeEach(() => (spectator = createComponent()));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     expect(spectator.component).toBeTruthy();
   });
 });
